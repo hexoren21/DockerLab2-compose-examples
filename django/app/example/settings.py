f05f014dbd733b229a4f376e9cea7b056007bcb4
hexoren21 <hexoren@gmail.com>
2024-10-08 10:40:26 +0200

    added django/compose logs

--- django/app/example/settings.py
+++ django/app/example/settings.py
@@ -89,6 +89,24 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
